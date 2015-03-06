0d8df5b304f0faa7db28fa6c72193c8e19690e7e
Dustin Frisch <fooker@lab.sh>
2015-03-06 17:54:27 +0100

    Refactoring

--- core/src/main/java/org/opennms/karaf/licencemanager/generate/LicenceArtifactsGenerator.java
+++ core/src/main/java/org/opennms/karaf/licencemanager/generate/LicenceArtifactsGenerator.java
@@ -15,15 +15,9 @@
 
 package org.opennms.karaf.licencemanager.generate;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-
 import org.opennms.karaf.licencemgr.GeneratedKeys;
+
+import java.io.*;
 
 public class LicenceArtifactsGenerator {
 
